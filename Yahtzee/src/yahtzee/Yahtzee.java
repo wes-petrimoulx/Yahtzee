1185427837e9495910746b6c9940f821c2d1d7cd
Wes <wpetrimoulx24@gmail.com>
2019-02-12 22:00:59 -0500

    fixed spelling

--- Yahtzee/src/yahtzee/Yahtzee.java
+++ Yahtzee/src/yahtzee/Yahtzee.java
@@ -10,7 +10,7 @@ public class Yahtzee {
 		
 		System.out.println("Welcome to Yahtzee!");
 		System.out.println("Rules: This game consists of 10 turns, trying to get the highest score at the end of those 10 turns.");
-		System.out.println("You will role 5 die at the start of your turn. You have the choice to add the sum of all 5 die that were");
+		System.out.println("You will roll 5 die at the start of your turn. You have the choice to add the sum of all 5 die that were");
 		System.out.println("rolled during your turn to your scorecard, or you may re-roll ONCE to try to get a higher score.");
 		System.out.println("Regardless if your re-roll score is higher or lower, it will be added to your scorecard.");
 		
